b24e897bcee600c50f8fd62b722e147c14b7d60f
brunocordioli <bcordiolimachado7@gmail.com>
2019-11-24 00:03:23 -0300

    Added files for auth0

--- centralerros/src/main/java/com/squaddois/centralerros/controller/UserController.java
+++ centralerros/src/main/java/com/squaddois/centralerros/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.squaddois.centralerros.controller;
 
+import com.auth0.Auth0;
 import com.squaddois.centralerros.dto.UserDTO;
+import com.squaddois.centralerros.security.Auth0Configuration;
 import com.squaddois.centralerros.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,15 +17,23 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final Auth0Configuration auth0Configuration;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, Auth0Configuration auth0Configuration) {
         this.userService = userService;
+        this.auth0Configuration = auth0Configuration;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findUser(@PathVariable Long id) {
         return new ResponseEntity<>(userService.findUser(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/gamer")
+    public ResponseEntity<?> getToken() {
+        Auth0 token = auth0Configuration.getAuth0();
+        return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
     @GetMapping("/findAll")
