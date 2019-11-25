863df1c6be8bda5954d9c6dbece6e4e1e12449c2
Gustavo Martins <gucmartins@gmail.com>
2019-11-25 15:34:48 -0300

    Include Swagger for API documentation

--- centralerros/src/main/java/com/squaddois/centralerros/config/WebSecurityConfigAdapter.java
+++ centralerros/src/main/java/com/squaddois/centralerros/config/WebSecurityConfigAdapter.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -41,4 +42,13 @@ public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(encoder.passwordEncoder());
     }
 
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
 }
