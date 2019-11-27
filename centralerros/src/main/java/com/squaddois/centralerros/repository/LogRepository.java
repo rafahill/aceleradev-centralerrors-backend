d60e20ae6b52e75920310bc65774415ebff50467
Gustavo Martins <gucmartins@gmail.com>
2019-11-27 14:29:55 -0300

    Log entity removed

--- centralerros/src/main/java/com/squaddois/centralerros/repository/LogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.squaddois.centralerros.repository;
-
-import com.squaddois.centralerros.entity.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogRepository extends JpaRepository<Log,Long> {
-}
