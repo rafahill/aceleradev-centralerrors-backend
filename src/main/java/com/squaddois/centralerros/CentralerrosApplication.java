0c2c3a69c39f87cf9c799210a855506462a9d02f
Rafael Hilgemberg <rberg@konselior.com>
2019-11-29 19:43:36 -0300

    projeto base pre refactor

--- src/main/java/com/squaddois/centralerros/CentralerrosApplication.java
+++ src/main/java/com/squaddois/centralerros/CentralerrosApplication.java
@@ -2,7 +2,13 @@ package com.squaddois.centralerros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +16,17 @@ public class CentralerrosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CentralerrosApplication.class, args);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("Origin", "x-test-header", "Content-Type", "User-Agent", "Referer", "DNT", "Accept", "Authorization", "x-requested-with"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
