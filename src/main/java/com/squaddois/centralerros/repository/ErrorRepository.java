d649ca964125fdb01d29ae06c9f9e7a393a9a404
Gustavo Martins <gucmartins@gmail.com>
2019-11-29 10:04:04 -0300

    Removing not used imports, dependencies and auth0.properties file

--- src/main/java/com/squaddois/centralerros/repository/ErrorRepository.java
+++ src/main/java/com/squaddois/centralerros/repository/ErrorRepository.java
@@ -3,8 +3,6 @@ package com.squaddois.centralerros.repository;
 import com.squaddois.centralerros.entity.Environment;
 import com.squaddois.centralerros.entity.Error;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
