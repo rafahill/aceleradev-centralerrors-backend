9b1103b680ec1272e52606be6805b38ae2a8223b
Rafael Hilgemberg <rberg@konselior.com>
2019-11-29 21:09:17 -0300

    refactor com auth0 funcionando

--- src/main/java/com/squaddois/centralerros/CentralerrosApplication.java
+++ src/main/java/com/squaddois/centralerros/CentralerrosApplication.java
@@ -1,14 +1,25 @@
 package com.squaddois.centralerros;
 
 import org.springframework.boot.SpringApplication;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -19,14 +30,43 @@ public class CentralerrosApplication {
 	}
 
 	@Bean
+	public ConfigurableServletWebServerFactory webServerFactory() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector ->
+				connector.setProperty("relaxedQueryChars", "|{}[]"));
+		return factory;
+	}
+
+	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedHeaders(Arrays.asList("Origin", "x-test-header", "Content-Type", "User-Agent", "Referer", "DNT", "Accept", "Authorization", "x-requested-with"));
+		configuration.setAllowedHeaders(Arrays.asList("Origin", "x-test-header", "Content-Type", "User-Agent", "Referer", "DNT", "Accept", "Authorization", "x-requested-with", "Kon-AuthId", "Kon-Seller", "Kon-Store", "Kon-Version"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
+				HttpClientBuilder.create().build());
+		RestTemplate template = new RestTemplate();
+		List<HttpMessageConverter<?>> converters = template.getMessageConverters();
+		for(HttpMessageConverter<?> converter : converters){
+			if(converter instanceof MappingJackson2HttpMessageConverter){
+				try{
+					List<MediaType> supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+					supportedMediaTypes.add(MediaType.TEXT_HTML);
+
+					((MappingJackson2HttpMessageConverter) converter).setSupportedMediaTypes(supportedMediaTypes);
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+			}
+		}
+		return template;
 	}
 
 }
